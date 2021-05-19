9c14afbdf453d8ebefd95dbf23af8e51a0a92020
Murilo Beltrame <murilobeltrame@gmail.com>
2021-05-19 00:09:27 -0300

    added mvc project

--- src/Example.Api/Controllers/WeatherForecastController.cs
+++ src/Example.Api/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace Example.Api.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Just called for Weather Forecast....");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
